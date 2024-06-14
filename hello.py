5c82171928e82da9b40b2252af25704bffc94664
vhly2000 <110129553+vhly2000@users.noreply.github.com>
2024-06-13 20:49:13 -0700

    continuation of the week 0

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("hello, world")
+#Ask user for their name
+name = input("What's your name? ").strip().title()
+
+#split user's name into first and last name 
+first, last = name.split(" ")
+
+#Say hello to user with their name
+print(f"Hello, {first}")
+
